ec4411b2197cc60ab75a28bc3a0c09a81074a534
liayzabel <65515002+liayzabel@users.noreply.github.com>
2020-07-31 14:56:15 +1200

    31/07/2020

--- computing_interface.py
+++ computing_interface.py
@@ -34,11 +34,11 @@ class Interface:
 
         
         # Name and year
-        Label(self.parent_window, text="Name:", bg="#00ffff", font=("Helvetica", 12, 'bold')).grid(row=2, column=1)
+        Label(self.parent_window, text="Name:", bg="#00ffff", padx=25, font=("Helvetica", 12, 'bold')).grid(row=2, column=1, sticky=E)
         self.name = Entry(self.parent_window, width=30)
         self.name.grid(row=2, column=2, columnspan=2)
 
-        Label(self.parent_window, text="Year:", bg="#00ffff", font=("Helvetica", 12, 'bold')).grid(row=3, column=1)
+        Label(self.parent_window, text="Year:", bg="#00ffff", padx=25, font=("Helvetica", 12, 'bold')).grid(row=3, column=1, sticky=E)
         self.year = Entry(self.parent_window, width=30)
         self.year.grid(row=3, column=2, columnspan=2)
         
@@ -58,8 +58,8 @@ class Interface:
         # Sending data to user class (creating an instance)
         self.student = Students(self.name, self.year) 
         
-        if self.year not in ["4","5","6"]:
-            messagebox.showerror("ERROR", "For year level, please enter a whole number from 4 to 6.")
+        if self.year not in ["6","7","8"]:
+            messagebox.showerror("ERROR", "For year level, please enter a whole number from 6 to 8. This program is intended for Year 6 to 8 only.")
             Interface.start(self)
 
         elif self.name == "":
@@ -74,21 +74,39 @@ class Interface:
         # destroying widgets from previous frame
         for widget in self.parent_window.winfo_children() :
             widget.destroy()
-
+        
         # Welcome text
         Label(self.parent_window, text="Ormiston Computing", bg="#00ffff", font=("Helvetica", 14, 'bold'), pady = 20).grid(row=1, column=1, columnspan = 3)
 
         # Buttons
         start_btn = Button(self.parent_window, text="Start", bg="#ffff00", font=("Helvetica", 14, 'bold'), width=8, command=self.option)
-        start_btn.grid(row=2, column=2)
+        start_btn.grid(row=2, column=2, rowspan=2)
 
         quit_btn = Button(self.parent_window, text="Quit", bg="#ff0000", font=("Helvetica", 12, 'bold'), width=6, command=self.quit)
-        quit_btn.grid(row=3, column=2)
+        quit_btn.grid(row=3, column=2, rowspan=2)
 
         # Footer text
-        help_link = Button(self.parent_window, text="Help?", bg="#00ffff", fg="#0000ff", borderwidth=0, font=("Helvetica", 8, 'underline'))
-        help_link.grid(row=5, column=1)
+        help_link = Button(self.parent_window, text="Help?", padx = 25, bg="#00ffff", fg="#0000ff", borderwidth=0, font=("Helvetica", 8, 'underline'), command=self.help)
+        help_link.grid(row=5, column=1, sticky=W)
+        
+        Label(self.parent_window, text="Creator : Thong & Lia", bg="#00ffff", font=("Helvetica", 8), pady = 20).grid(row=5, column=3)
 
+
+    def help(self):
+        # destroying widgets from previous frame
+        for widget in self.parent_window.winfo_children() :
+            widget.destroy()
+
+        # Header "instructions" text
+        Label(self.parent_window, text="Instructions:", padx=25, pady=25, bg="#00ffff", borderwidth=0, font=("Helvetica", 14, 'bold')).grid(row=1, column=1)
+        home_btn = Button(self.parent_window, text="Home", bg="#abab0a", font=("Helvetica", 10, 'bold'), width=9, command=self.home)
+        home_btn.grid(row=1, column=3)
+
+        Label(self.parent_window, bg="#00ffff", width = 25, text='''Simply type your answer to the question in the text box below the question then click Submit. \n
+        The program will then show you if you are correct or not. Click next to answer the next question. Example Below:''', font=("Helvetica", 14, 'bold')).grid(row=2, column=1, columnspan=3)
+
+        
+        
     def option(self):
         # destroying widgets from previous frame
         for widget in self.parent_window.winfo_children() :
@@ -96,17 +114,19 @@ class Interface:
             
         # Minimum row size
         self.parent_window.rowconfigure(2, minsize=10)
+        self.parent_window.rowconfigure(3, minsize=60)
+        self.parent_window.rowconfigure(4, minsize=60)
 
         # Welcome, [student] text
         Label(self.parent_window, text="Welcome, {}".format(self.name), bg="#00ffff", font=("Helvetica", 14, 'bold'), pady = 20).grid(row=1, column=1, columnspan = 3)
 
         # Question
         Label(self.parent_window, text="Which topic would you like to practice?", bg="#00ffff", font=("Helvetica", 12, 'bold')).grid(row=2, column=1, columnspan = 3)
-
+        
         # Buttons
         addition_btn = Button(self.parent_window, text="Addition [+]", bg="#ffff00", font=("Helvetica", 10, 'bold'), pady = 8, padx = 35, width=8)
         addition_btn.grid(row=3, column=2)
-
+        
         multiplication_btn = Button(self.parent_window, text="Multiplication [x]", bg="#ffff00", font=("Helvetica", 10, 'bold'), pady = 8, padx = 35, width=8)
         multiplication_btn.grid(row=4, column=2)
         
